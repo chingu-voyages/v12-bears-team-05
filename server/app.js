757a9b54c17396bd3109c2b0513abb08fa433272
jasmeetsohal <jasmeetsohal93@gmail.com>
2019-10-21 15:23:39 +0530

    implement JWT authentication

--- server/app.js
+++ server/app.js
@@ -1,53 +1,43 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-const MongoClient = require("mongodb").MongoClient;
-var mongo = require("./config/mongo");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-// var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const appRoute = require('./routes/index');
 
-var app = express();
+dotenv.config();
 
-// Connection URL
-const url = "mongodb://localhost:27017";
+const app = express();
+
 
 // Database Name
 const dbName = "MealTracker";
 
 // Use connect method to connect to the server
-MongoClient.connect(
-  url,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    if (err) {
-      console.log("Failed to connect mongo :: ", err);
-    } else {
-      console.log("Connected successfully to server");
-
-      const db = client.db(dbName);
-      mongo.setMongo(db);
-    }
-
-    //   client.close();
-  }
-);
+mongoose.connect(
+  process.env.DB_CONNECT + '/' + dbName, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  },
+  () => console.log("Connected to DB!"));
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
-app.use("/users", usersRouter);
+app.use("/api", appRoute);
 
-console.log("NODE_ENV", process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.join(__dirname, "../build")));
 
   // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../build", "index.html"));
   });
 }
---
+++ server/routes/auth.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {
+    registerValidation,
+    loginValidation
+} = require('../validation');
+
+//Register
+router.post('/register', async (req, res) => {
+
+    //validating user
+    const {
+        error
+    } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //checking email already exist
+    const emailExist = await User.findOne({
+        email: req.body.email
+    });
+    if (emailExist) return res.status(400).send('Email already exist');
+
+    //hasing password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashPassword
+    });
+
+    try {
+        const saveUser = await user.save();
+        res.send({
+            user: saveUser._id
+        });
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+//Login
+router.post('/login', async (req, res) => {
+    //validating login
+    const {
+        error
+    } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+     //checking email already exist
+     const user = await User.findOne({
+        email: req.body.email
+    });
+    if (!user) return res.status(400).send('Email or password is incorrect');
+
+    //password
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Invalid password');
+
+    const token = jwt.sign({_id:user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token',token).send(token);
+
+});
+
+module.exports = router;
