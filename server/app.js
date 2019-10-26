d86e6cd252c92282c9963395e13a5878e5fffcfd
vikneshwar <lviknesh@gmail.com>
2019-10-26 05:56:38 +0530

    update apis

--- server/app.js
+++ server/app.js
@@ -2,32 +2,50 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const appRoute = require('./routes/index');
+const appRoute = require("./routes/index");
 
 dotenv.config();
 
 const app = express();
 
-
 // Database Name
 const dbName = "MealTracker";
 
 // Use connect method to connect to the server
+console.log("DB", process.env.DB_CONNECT);
 mongoose.connect(
-  process.env.DB_CONNECT + '/' + dbName, {
+  process.env.DB_CONNECT + "/" + dbName,
+  {
     useUnifiedTopology: true,
     useNewUrlParser: true
   },
-  () => console.log("Connected to DB!"));
+  error =>
+    console.log(
+      `${error ? `Error connecting to DB ${error}` : `Connected to DB`}`
+    )
+);
+
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === "development") {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+  }
+  next();
+});
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
 app.use(cookieParser());
 
 app.use("/api", appRoute);
@@ -37,7 +55,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../build")));
 
   // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../build", "index.html"));
   });
 }
--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,25 +1,24 @@
-const User = require('../model/User');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const {
-    registerValidation,
-    loginValidation
-} = require('../validation');
-
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const pick = require("lodash").pick;
+const { registerValidation, loginValidation } = require("../validation");
 
 const register = async (req, res) => {
-
-    //validating user
-    const {
-        error
-    } = registerValidation(req.body);
+  //validating user
+  try {
+    const { error } = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     //checking email already exist
     const emailExist = await User.findOne({
-        email: req.body.email
+      email: req.body.email
     });
-    if (emailExist) return res.status(400).send('Email already exist');
+    if (emailExist)
+      return res.status(400).send({
+        success: false,
+        message: "Email already exist"
+      });
 
     //hasing password
     const salt = await bcrypt.genSalt(10);
@@ -27,43 +26,68 @@ const register = async (req, res) => {
 
     //create user
     const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashPassword
+      name: req.body.name,
+      email: req.body.email,
+      password: hashPassword
     });
 
-    try {
-        const saveUser = await user.save();
-        res.send({
-            user: saveUser._id
-        });
-    } catch (err) {
-        res.status(400).send(err);
-    }
-}
+    const saveUser = await user.save();
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
 
+    const userResp = pick(saveUser, ["name", "email"]);
+
+    res.header("auth-token", token).send({
+      success: true,
+      user: userResp,
+      token
+    });
+  } catch (err) {
+    res.status(400).send({
+      success: false,
+      message: err.message
+    });
+  }
+};
 
 const login = async (req, res) => {
-    //validating login
-    const {
-        error
-    } = loginValidation(req.body);
+  //validating login
+  try {
+    const { error } = loginValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-     //checking email already exist
-     const user = await User.findOne({
-        email: req.body.email
+    //checking email already exist
+    const user = await User.findOne({
+      email: req.body.email
     });
-    if (!user) return res.status(400).send('Email or password is incorrect');
+    if (!user)
+      return res.status(400).send({
+        success: false,
+        message: "User does not exist"
+      });
 
     //password
     const validPass = await bcrypt.compare(req.body.password, user.password);
-    if(!validPass) return res.status(400).send('Invalid password');
+    if (!validPass)
+      return res.status(400).send({
+        success: false,
+        message: "Invalid password"
+      });
 
-    const token = jwt.sign({_id:user._id}, process.env.TOKEN_SECRET);
-    res.header('auth-token',token).send(token);
+    const userResp = pick(user, ["name", "email"]);
 
-}
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header("auth-token", token).send({
+      success: true,
+      user: userResp,
+      token
+    });
+  } catch (err) {
+    res.status(400).send({
+      succes: true,
+      message: err.message
+    });
+  }
+};
 
 module.exports.register = register;
 module.exports.login = login;
