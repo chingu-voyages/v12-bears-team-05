4aeb948d3daa2672ff1dd6d2705bd3fbc2e10171
vikneshwar <lviknesh@gmail.com>
2019-11-04 04:26:03 +0530

    refactor topbar

--- src/containers/Root/index.js
+++ src/containers/Root/index.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react';
-import { Switch, Route, useHistory } from 'react-router-dom';
-import { CssBaseline, Container } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/styles';
-import theme from '../../utils/theme';
-import LoginSignUp from '../LoginSignUp';
-import TopBar from '../../components/TopBar';
-import CreateRecipe from '../CreateRecipe';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectIsLoggedIn, selectIsLoading } from './selector';
-import { getProfileAction, logoutAction } from './actions';
+import React, { useEffect } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
+import { CssBaseline, Container } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../../utils/theme";
+import LoginSignUp from "../LoginSignUp";
+import TopBar from "../../components/TopBar";
+import CreateRecipe from "../CreateRecipe";
+import { useSelector, useDispatch } from "react-redux";
+import { selectIsLoggedIn, selectIsLoading } from "./selector";
+import { getProfileAction, logoutAction } from "./actions";
 
 export default function App() {
   const isLoggedIn = useSelector(selectIsLoggedIn);
@@ -17,11 +17,11 @@ export default function App() {
   const dispatch = useDispatch();
 
   const onLogout = () => dispatch(logoutAction());
-  const onLoginClick = () => history.push('/login');
-  const onHome = () => history.push('/');
+  const onLoginClick = () => history.push("/login");
+  const onHome = () => history.push("/");
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (!token) return;
 
     dispatch(getProfileAction());
@@ -35,14 +35,12 @@ export default function App() {
         onLogout={onLogout}
         onLogin={onLoginClick}
         onHome={onHome}
-        inLoginPage={window.location.pathname === '/login'}
+        inLoginPage={window.location.pathname === "/login"}
       />
       <section className="container">
         <CssBaseline />
         <Container maxWidth="lg">
           <Switch>
-            <Route exact path="/" component={() => <div></div>} />
-            <Route path="/login" component={LoginSignUp} />
             <Route path="/create-recipe" component={CreateRecipe} />
             <Route exact path="/">
               <div>Home Page</div>
